cd333a058339252e2969ee4ea56d076b7a9eab7d
Diana M <dmmiller85@gmail.com>
2019-10-06 23:57:49 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,39 @@
 # My Code here....
+def map_to_negativize(source_array)
+ while i < source_array.length do
+     new.push(source_array[i] * -1)
+     i += 1
+end
+
+def map_to_no_change(source_array)
+  new = []
+  while i < source_array.length do
+     new.push(source_array[i])
+     i += 1
+end
+
+def map_to_double(source_array)
+  new = []
+  while i < source_array.length do
+     new.push(source_array[i] * 2)
+     i += 1
+end
+
+def map_to_square(source_array)
+    new = []
+  while i < source_array.length do
+     new.push(source_array[i] **)
+     i += 1
+end
+
+def reduce_to_total(source_array, starting_point)
+  
+end
+
+def redue_to_all_true(source_array)
+  
+end
+
+def redue_to_any_true(source_array)
+  
+end
